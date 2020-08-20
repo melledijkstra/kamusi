ec4c108f0b39866f355db1bc849672091d997eef
Melle Dijkstra <dev.melle@gmail.com>
2020-08-20 14:56:11 +0200

    Updates

---
+++ precache-manifest.7da0f0854a74d974320d72d2d6fe1144.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a4e98220f83e5cacd42739a10a15231",
+    "url": "/kamusi/index.html"
+  },
+  {
+    "revision": "4a95fdb7c9653dcc023a",
+    "url": "/kamusi/static/css/2.04ff9426.chunk.css"
+  },
+  {
+    "revision": "53f66aee487f6404c056",
+    "url": "/kamusi/static/css/main.d4d0d634.chunk.css"
+  },
+  {
+    "revision": "4a95fdb7c9653dcc023a",
+    "url": "/kamusi/static/js/2.b374fd3d.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/kamusi/static/js/2.b374fd3d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "53f66aee487f6404c056",
+    "url": "/kamusi/static/js/main.fc0c7228.chunk.js"
+  },
+  {
+    "revision": "697b1bc0e41e6fd8dbb5",
+    "url": "/kamusi/static/js/runtime-main.8414f68e.js"
+  }
+]);
